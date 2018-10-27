21387cfc7eadc56e97a30b40ebe0a9b2cab53cf0
Kafeyin Z <kafeyin@gmail.com>
2018-10-27 22:05:05 +0300

    küçük değişiklikler ...

--- app_product/models.py
+++ app_product/models.py
@@ -1,6 +1,8 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
 
+
+# FIXME: Yüklenen resim boyutu sınırı yok
 class Product(models.Model):
     title = models.CharField(null=True, blank=False, max_length=100)
     content = models.TextField(null=True, blank=False, max_length=1000)
@@ -9,7 +11,7 @@ class Product(models.Model):
     update_date = models.DateTimeField(auto_now=True)
     active = models.BooleanField(default=False)
     outline = models.BooleanField(default=False)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
 
     # Template de kullanımı {{ post.get_image }}
     def get_image(self):
